b77c036ef5fca148bc9e02eda210b25fda509c46
wwjll <jjialin92@gmail.com>
2021-01-21 15:49:48 +0800

    1.添加 perlin 火球 测试

--- universe.js
+++ universe.js
@@ -12,12 +12,206 @@ function getHeight(obj) {
   let y_max = y_arr.sort((a, b) => { return a - b})[y_arr.length - 1]
   let y_min = y_arr.sort((a, b) => { return b - a})[y_arr.length - 1]
   let scale_y = obj.scale.y
+
   // 物体实际高度
   let height = Math.abs(y_max - y_min) * scale_y
   // 获得 y 切面最小的值, 该值如果小于 0, 则证明该几何体部分位于原点以下, 需要抬升相应的高度
   let y_diff = y_min < 0 ? Math.abs(y_min) * scale_y : 0
 
   return y_diff
+}
+
+let material
+function fire(group){
+  const vertexShader = 
+    `
+    //
+    // GLSL textureless classic 3D noise "cnoise",
+    // with an RSL-style periodic variant "pnoise".
+    // Author:  Stefan Gustavson (stefan.gustavson@liu.se)
+    // Version: 2011-10-11
+    //
+    // Many thanks to Ian McEwan of Ashima Arts for the
+    // ideas for permutation and gradient selection.
+    //
+    // Copyright (c) 2011 Stefan Gustavson. All rights reserved.
+    // Distributed under the MIT license. See LICENSE file.
+    // https://github.com/ashima/webgl-noise
+    //
+
+    vec3 mod289(vec3 x)
+    {
+        return x - floor(x * (1.0 / 289.0)) * 289.0;
+    }
+
+    vec4 mod289(vec4 x)
+    {
+        return x - floor(x * (1.0 / 289.0)) * 289.0;
+    }
+
+    vec4 permute(vec4 x)
+    {
+        return mod289(((x*34.0)+1.0)*x);
+    }
+
+    vec4 taylorInvSqrt(vec4 r)
+    {
+        return 1.79284291400159 - 0.85373472095314 * r;
+    }
+
+    vec3 fade(vec3 t) {
+        return t*t*t*(t*(t*6.0-15.0)+10.0);
+    }
+    
+
+    // Classic Perlin noise, periodic variant
+    float pnoise(vec3 P, vec3 rep)
+    {
+    vec3 Pi0 = mod(floor(P), rep); // Integer part, modulo period
+    vec3 Pi1 = mod(Pi0 + vec3(1.0), rep); // Integer part + 1, mod period
+    Pi0 = mod289(Pi0);
+    Pi1 = mod289(Pi1);
+    vec3 Pf0 = fract(P); // Fractional part for interpolation
+    vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
+    vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+    vec4 iy = vec4(Pi0.yy, Pi1.yy);
+    vec4 iz0 = Pi0.zzzz;
+    vec4 iz1 = Pi1.zzzz;
+
+    vec4 ixy = permute(permute(ix) + iy);
+    vec4 ixy0 = permute(ixy + iz0);
+    vec4 ixy1 = permute(ixy + iz1);
+
+    vec4 gx0 = ixy0 * (1.0 / 7.0);
+    vec4 gy0 = fract(floor(gx0) * (1.0 / 7.0)) - 0.5;
+    gx0 = fract(gx0);
+    vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+    vec4 sz0 = step(gz0, vec4(0.0));
+    gx0 -= sz0 * (step(0.0, gx0) - 0.5);
+    gy0 -= sz0 * (step(0.0, gy0) - 0.5);
+
+    vec4 gx1 = ixy1 * (1.0 / 7.0);
+    vec4 gy1 = fract(floor(gx1) * (1.0 / 7.0)) - 0.5;
+    gx1 = fract(gx1);
+    vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+    vec4 sz1 = step(gz1, vec4(0.0));
+    gx1 -= sz1 * (step(0.0, gx1) - 0.5);
+    gy1 -= sz1 * (step(0.0, gy1) - 0.5);
+
+    vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
+    vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
+    vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
+    vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
+    vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
+    vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
+    vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
+    vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
+
+    vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+    g000 *= norm0.x;
+    g010 *= norm0.y;
+    g100 *= norm0.z;
+    g110 *= norm0.w;
+    vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+    g001 *= norm1.x;
+    g011 *= norm1.y;
+    g101 *= norm1.z;
+    g111 *= norm1.w;
+
+    float n000 = dot(g000, Pf0);
+    float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+    float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+    float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+    float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+    float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+    float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+    float n111 = dot(g111, Pf1);
+
+    vec3 fade_xyz = fade(Pf0);
+    vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+    vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+    float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x);
+    return 2.2 * n_xyz;
+    }
+
+    // Include the Ashima code here!
+
+    varying vec2 vUv;
+    varying float noise;
+    uniform float time;
+
+    float turbulence( vec3 p ) {
+    // float w = 100.0;
+    float t = -.5;
+    for (float f = 1.0 ; f <= 10.0 ; f++ ){  // 凸起的数量
+        float power = pow( 2.0, f );
+        t += abs( pnoise( vec3( power * p ), vec3( 10.0, 10.0, 10.0 ) ) / power );
+    }
+    return t;
+    }
+
+    void main() {
+
+    vUv = uv;
+
+    noise = 10.0 *  -.10 * turbulence( .5 * normal + time );
+    float b = 5.0 * pnoise( 0.05 * position + vec3( 2.0 * time ), vec3( 100.0 ) );
+    float displacement = - 10. * noise + b;
+
+    // float displacement = - 10. * noise + time; // 波浪起伏度
+
+    vec3 newPosition = position + normal * displacement;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( newPosition, 1.0 );
+
+    }
+    `
+  const fragmentShader = 
+  `
+    varying vec2 vUv;
+    varying float noise;
+    uniform sampler2D tExplosion;
+    // uniform float time;
+
+    float random( vec3 scale, float seed ){
+    return fract( sin( dot( gl_FragCoord.xyz + seed, scale ) ) * 43758.5453 + seed ) ;
+    }
+
+    void main() {
+
+    float r = .01 * random( vec3( 12.9898, 78.233, 151.7182 ), 0.0 );
+    vec2 tPos = vec2( 0, 1.3 * noise + r );
+    vec4 color = texture2D( tExplosion, tPos );
+    gl_FragColor = vec4( color.rgb, 1.0 );
+
+    }
+  `
+
+  const texture = new THREE.TextureLoader().load('texture.webp')
+  // 创建一个 shader 材质
+  material = new THREE.ShaderMaterial({
+      uniforms: {
+          tExplosion: {
+              type: "t",
+              value: texture
+          },
+          time: {
+              type: "f",
+              value: 0.0
+          }
+      },
+      vertexShader: vertexShader,
+      fragmentShader: fragmentShader
+  })
+  
+  // 创建一个球体并贴上材质
+  mesh = new THREE.Mesh(
+      new THREE.IcosahedronGeometry(20, 4 ),
+      material
+  )
+  mesh.scale.set(500, 500, 500)
+  mesh.position.y += getHeight(mesh)
+  // mesh.position.y += 5000
+  group.add(mesh)
 }
 
 function Universe() {
@@ -117,7 +311,6 @@ Object.assign(Universe.prototype, {
     let axes = new THREE.AxesHelper(15000)
     // axes.position.y += 5000
     group.add(axes)
-    console.log(axes.position)
 
     group.center = center
     // 基平面法向量
@@ -132,7 +325,6 @@ Object.assign(Universe.prototype, {
     console.log(getHeight(mesh))
     // 由于旋转后将 y 轴作为球面法向量, 此时几何体地
     mesh.position.y += getHeight(mesh)
-    console.log(mesh.geometry)
     group.add(mesh)
   },
   createPlane: function(boundary, flyto) {
@@ -230,10 +422,15 @@ Object.assign(Universe.prototype, {
 
 
   renderLoop: function() {
-    // console.log(Object.getOwnPropertyNames(this))
     requestAnimationFrame(this.renderLoop.bind(this))
     this.cesium.viewer.render()
     this.syncThree()
+    try {
+      if(!material) return
+      material.uniforms[ 'time' ].value += .0025
+    } catch(e) {
+      console.log(e)
+    }
   }
   
 })
@@ -264,7 +461,14 @@ U.collide().then(data => {
   let latheMesh = new THREE.Mesh(geometry, doubleSideMaterial)
   window.mesh = latheMesh
   latheMesh.scale.set(500, 500, 500)
-  U.modifyThreeObj(latheMesh, group)
+  // 
+  // U.modifyThreeObj(latheMesh, group)
+  //
+  try {
+    fire(group)
+  } catch (e) {
+    console.log(e)
+  }
   // 帧更新
   U.renderLoop()
 })
